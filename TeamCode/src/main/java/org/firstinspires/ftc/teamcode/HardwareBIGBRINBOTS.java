360ffecf1dc563c51005b489d47aff371e7d3996
Ping Bai <56081730+FTCPrep@users.noreply.github.com>
2021-11-12 12:30:00 -0500

    Add HardwareBIGBRINBOTS class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareBIGBRINBOTS.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class HardwareBIGBRINBOTS {
+    public DcMotor FrontLeftDrive = null;
+    public DcMotor FrontRightDrive = null;
+    public DcMotor RearLeftDrive = null;
+    public DcMotor RearRightDrive = null;
+    public DcMotor Flywheel = null;
+    /* local Opmode members. */
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareBIGBRINBOTS(){
+
+    }
+
+    public void init(HardwareMap ahwMap){
+        //save reference to Hardware map
+        hwMap = ahwMap;
+        //Define and initialize motors
+        FrontLeftDrive = hwMap.get(DcMotor.class, "FL_DCmotor");
+        FrontRightDrive = hwMap.get(DcMotor.class, "FR_DCmotor");
+        RearLeftDrive = hwMap.get(DcMotor.class,"RL_DCmotor");
+        RearRightDrive = hwMap.get(DcMotor.class,"RR_DCmotor");
+        Flywheel = hwMap.get(DcMotor.class, "Flywheel");
+
+        FrontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        FrontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        RearLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        RearRightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Set all motors to zero power
+        FrontLeftDrive.setPower(0);
+        FrontRightDrive.setPower(0);
+        RearLeftDrive.setPower(0);
+        RearRightDrive.setPower(0);
+        Flywheel.setPower(0);
+
+        FrontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FrontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        RearLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        RearRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+}

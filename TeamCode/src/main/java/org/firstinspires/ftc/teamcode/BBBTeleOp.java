6c07dacfe6fa14a4b05688d0cb49b063373579de
ethaowe <77026818+ethaowe@users.noreply.github.com>
2021-11-13 10:58:23 -0500

    New motor intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBBTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBBTeleOp.java
@@ -5,43 +5,52 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
+@TeleOp
 public class BBBTeleOp extends LinearOpMode {
     HardwareBIGBRAINBOTS robot   = new HardwareBIGBRAINBOTS();   // Use BIGBRAINBOTS's hardware
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(this.hardwareMap);
+
         telemetry.addData("Mode", "waiting");
         telemetry.update();
 
         waitForStart();
-
         while (opModeIsActive()) {
             double drive = gamepad1.left_stick_y;
-            double strafe =-gamepad1.right_stick_x;
-            double turn =-gamepad1.left_stick_x;//changed to negative to fix problem with turning the wrong direction
+            double strafe = -gamepad1.right_stick_x;
+            double turn = -gamepad1.left_stick_x;
+            boolean turncarousel = gamepad1.x;
+            boolean turnIntake = gamepad1.a;
+            boolean reverseIntake = gamepad1.b;
             double FLPower = Range.clip(drive + strafe + turn, -1.0, 1.0);
             double FRPower = Range.clip(drive - strafe - turn, -1.0, 1.0);
             double BLPower = Range.clip(drive - strafe + turn, -1.0, 1.0);
             double BRPower = Range.clip(drive + strafe - turn, -1.0, 1.0);
-            boolean wheelSpin = gamepad2.a;
+
+
             robot.FrontLeftDrive.setPower(FLPower);
             robot.FrontRightDrive.setPower(FRPower);
             robot.RearLeftDrive.setPower(BLPower);
             robot.RearRightDrive.setPower(BRPower);
+            if (turncarousel){
+                robot.CarouselMotor.setPower(1);
+            }else{
+                robot.CarouselMotor.setPower(0);
+            }
+            if (turnIntake){
+                robot.IntakeMotor.setPower(.2);
+            }else if(reverseIntake){
+                robot.IntakeMotor.setPower(-0.2);
+            }else{
+                robot.IntakeMotor.setPower(0);
+            }
 
             telemetry.addData("Mode", "Running");
             telemetry.addData("Power", "Frontleft=%.3f, Frontright=%.3f", FLPower, FRPower);
             telemetry.addData("Power", "Backleft=%.3f, Backright=%.3f", BLPower, BRPower);
-            telemetry.update();
 
-            if (wheelSpin){
-                robot.Flywheel.setPower(1);
-            }
-            if (wheelSpin == false){
-                robot.Flywheel.setPower(0);
-            }
-            sleep(69);
             idle();
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareBIGBRAINBOTS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareBIGBRAINBOTS.java
@@ -9,42 +9,208 @@ public class HardwareBIGBRAINBOTS {
     public DcMotor FrontRightDrive = null;
     public DcMotor RearLeftDrive = null;
     public DcMotor RearRightDrive = null;
-    public DcMotor Flywheel = null;
-    /* local Opmode members. */
+    public DcMotor CarouselMotor = null;
+    public DcMotor IntakeMotor = null;
+
     HardwareMap hwMap = null;
     private ElapsedTime period = new ElapsedTime();
 
-    /* Constructor */
-    public HardwareBIGBRAINBOTS(){
+    public HardwareBIGBRAINBOTS () {
 
     }
 
-    public void init(HardwareMap ahwMap){
-        //save reference to Hardware map
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
         hwMap = ahwMap;
-        //Define and initialize motors
+        // Define and Initialize Motors
         FrontLeftDrive = hwMap.get(DcMotor.class, "FL_DCmotor");
         FrontRightDrive = hwMap.get(DcMotor.class, "FR_DCmotor");
-        RearLeftDrive = hwMap.get(DcMotor.class,"RL_DCmotor");
-        RearRightDrive = hwMap.get(DcMotor.class,"RR_DCmotor");
-        Flywheel = hwMap.get(DcMotor.class, "Flywheel");
+        RearLeftDrive = hwMap.get(DcMotor.class, "RL_DCmotor");
+        RearRightDrive = hwMap.get(DcMotor.class, "RR_DCmotor");
+        CarouselMotor = hwMap.get(DcMotor.class, "CarouselMotor");
+        IntakeMotor = hwMap.get(DcMotor.class, "IntakeMotor");
+
+
 
         FrontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         FrontRightDrive.setDirection(DcMotor.Direction.REVERSE);
         RearLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         RearRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        CarouselMotor.setDirection(DcMotor.Direction.FORWARD);
+        IntakeMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        // Set all motors to zero power
         FrontLeftDrive.setPower(0);
         FrontRightDrive.setPower(0);
         RearLeftDrive.setPower(0);
         RearRightDrive.setPower(0);
-        Flywheel.setPower(0);
+        CarouselMotor.setPower(0);
+        IntakeMotor.setPower(0);
 
         FrontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         RearLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         RearRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        CarouselMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        IntakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        FrontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FrontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RearLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RearRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        CarouselMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        IntakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        FrontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FrontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RearLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RearRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        CarouselMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        IntakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void drive(double power, int EncoderCounts) {
+        power *= -1;
+        FrontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FrontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RearLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RearRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FrontLeftDrive.setTargetPosition(EncoderCounts);
+        FrontRightDrive.setTargetPosition(EncoderCounts);
+        RearLeftDrive.setTargetPosition(EncoderCounts);
+        RearRightDrive.setTargetPosition(EncoderCounts);
+        FrontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RearLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RearRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontLeftDrive.setPower(power);
+        FrontRightDrive.setPower(power);
+        RearLeftDrive.setPower(power);
+        RearRightDrive.setPower(power);
+        while (FrontLeftDrive.isBusy() || FrontRightDrive.isBusy() || RearLeftDrive.isBusy() || RearRightDrive.isBusy()) {
+            //telemetry.addData("Path0", "Starting at %7d :%7d :%7d :%7d",
+            //        FrontLeftDrive.getCurrentPosition(),
+            //            FrontRightDrive.getCurrentPosition(),
+            //          RearLeftDrive.getCurrentPosition(),
+            //        RearRightDrive.getCurrentPosition());
+            //telemetry.update();
+        }
+        FrontLeftDrive.setPower(0);
+        FrontRightDrive.setPower(0);
+        RearLeftDrive.setPower(0);
+        RearRightDrive.setPower(0);
+
+        FrontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FrontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RearLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RearRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void strafe(double power, int EncoderCounts) {
+        FrontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FrontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RearLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RearRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        FrontLeftDrive.setTargetPosition(-1 * EncoderCounts);
+        FrontRightDrive.setTargetPosition(EncoderCounts);
+        RearLeftDrive.setTargetPosition(EncoderCounts);
+        RearRightDrive.setTargetPosition(-1 * EncoderCounts);
+        FrontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RearLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RearRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontLeftDrive.setPower(-1 * power);
+        FrontRightDrive.setPower(power);
+        RearLeftDrive.setPower(power);
+        RearRightDrive.setPower(-1 * power);
+        while (FrontLeftDrive.isBusy() || FrontRightDrive.isBusy() || RearLeftDrive.isBusy() || RearRightDrive.isBusy()) {
+            //telemetry.addData("Path0", "Starting at %7d :%7d :%7d :%7d",
+            //        FrontLeftDrive.getCurrentPosition(),
+            //            FrontRightDrive.getCurrentPosition(),
+            //          RearLeftDrive.getCurrentPosition(),
+            //        RearRightDrive.getCurrentPosition());
+            //telemetry.update();
+        }
+        FrontLeftDrive.setPower(0);
+        FrontRightDrive.setPower(0);
+        RearLeftDrive.setPower(0);
+        RearRightDrive.setPower(0);
+
+        FrontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FrontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RearLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RearRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //telemetry.addData("Motors", "left (%.2f), right (%.2f)", 0,power);
+    }
+
+    public void turn(double power, int EncoderCounts) {
+        FrontLeftDrive.setTargetPosition(-1 * EncoderCounts);
+        FrontRightDrive.setTargetPosition(EncoderCounts);
+        RearLeftDrive.setTargetPosition(-1 * EncoderCounts);
+        RearRightDrive.setTargetPosition(EncoderCounts);
+        FrontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RearLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RearRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontLeftDrive.setPower(-1 * power);
+        FrontRightDrive.setPower(power);
+        RearLeftDrive.setPower(-1 * power);
+        RearRightDrive.setPower(power);
+        while (FrontLeftDrive.isBusy() || FrontRightDrive.isBusy() || RearLeftDrive.isBusy() || RearRightDrive.isBusy()) {
+            //telemetry.addData("Path0", "Starting at %7d :%7d :%7d :%7d",
+            //        FrontLeftDrive.getCurrentPosition(),
+            //            FrontRightDrive.getCurrentPosition(),
+            //          RearLeftDrive.getCurrentPosition(),
+            //        RearRightDrive.getCurrentPosition());
+            //telemetry.update();
+        }
+        FrontLeftDrive.setPower(0);
+        FrontRightDrive.setPower(0);
+        RearLeftDrive.setPower(0);
+        RearRightDrive.setPower(0);
+        FrontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FrontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RearLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RearRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FrontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FrontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RearLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RearRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void turncarousel(double power, int EncoderCounts) {
+        CarouselMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        CarouselMotor.setTargetPosition(EncoderCounts);
+        CarouselMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        CarouselMotor.setPower(power);
+        while (CarouselMotor.isBusy()) {
+            //telemetry.addData("Path0", "Starting at %7d :%7d :%7d :%7d",
+            //        FrontLeftDrive.getCurrentPosition(),
+            //            FrontRightDrive.getCurrentPosition(),
+            //          RearLeftDrive.getCurrentPosition(),
+            //        RearRightDrive.getCurrentPosition());
+            //telemetry.update();
+        }
+        CarouselMotor.setPower(0);
+        CarouselMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void runIntake(double power, int EncoderCounts) {
+        IntakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        IntakeMotor.setTargetPosition(EncoderCounts);
+        IntakeMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        IntakeMotor.setPower(power);
+        //CarouselMotor.setPower(0);
+        CarouselMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void stopIntake() {
+        IntakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        IntakeMotor.setTargetPosition(0);
+        IntakeMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        CarouselMotor.setPower(0);
+        CarouselMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
 }
